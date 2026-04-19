7ec93e8d521bb6bfd71f9cc2c87ac49d37266009
ZielaM <izielinski379@gmail.com>
2026-04-19 21:11:44 +0200

    added prisma

---
+++ prisma/migrations/20260419190036_news_setup/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "News" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "News_pkey" PRIMARY KEY ("id")
+);
---
+++ src/lib/prisma.ts
@@ -0,0 +1,24 @@
+// src/lib/prisma.ts
+import { PrismaClient } from '@/generated/prisma/client';
+import { neonConfig } from '@neondatabase/serverless';
+import { PrismaNeon } from '@prisma/adapter-neon';
+import ws from 'ws';
+
+neonConfig.webSocketConstructor = ws;
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaNeon({ connectionString });
+
+const prismaClientSingleton = () => {
+  return new PrismaClient({ adapter });
+};
+
+declare global {
+  // eslint-disable-next-line no-var
+  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+export const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;

462a37800711cdf8cd02807950c17234e5fe0a32
ZielaM <izielinski379@gmail.com>
2026-05-27 19:13:30 +0200

    Added loading page as a fallback for SSG

---
+++ src/app/[locale]/about-us/structure/[team]/[member]/loading.tsx
@@ -0,0 +1,31 @@
+import pageStyle from './page.module.scss';
+import skeletonStyle from './loading.module.scss';
+import clsx from 'clsx';
+
+export default function Loading() {
+  return (
+    <div className={pageStyle.page}>
+      <div className={clsx(skeletonStyle.skeleton, skeletonStyle.backLink)} />
+
+      <div className={clsx(skeletonStyle.skeleton, skeletonStyle.heroCard)}>
+        <div className={clsx(skeletonStyle.skeleton, skeletonStyle.avatar)} />
+        <div className={skeletonStyle.heroLines}>
+          <div
+            className={clsx(skeletonStyle.skeleton, skeletonStyle.heroLine1)}
+          />
+          <div
+            className={clsx(skeletonStyle.skeleton, skeletonStyle.heroLine2)}
+          />
+          <div
+            className={clsx(skeletonStyle.skeleton, skeletonStyle.heroLine3)}
+          />
+        </div>
+      </div>
+
+      <div className={skeletonStyle.infoGrid}>
+        <div className={clsx(skeletonStyle.skeleton, skeletonStyle.infoCard)} />
+        <div className={clsx(skeletonStyle.skeleton, skeletonStyle.infoCard)} />
+      </div>
+    </div>
+  );
+}

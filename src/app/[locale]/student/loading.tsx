a89e060f006330bc7802abbcd4c4509c6dbad79b
ZielaM <izielinski379@gmail.com>
2026-08-30 20:59:59 +0200

    Added proper loading screen

--- src/app/[locale]/student/loading.tsx
+++ src/app/[locale]/student/loading.tsx
@@ -4,12 +4,42 @@ import clsx from 'clsx';
 
 export default function Loading() {
   const rowSkeletons = Array.from({ length: 5 });
+  const announcementSkeletons = Array.from({ length: 2 });
 
   return (
     <div className={style.page}>
       <div
         className={clsx(skeletonStyle.skeleton, skeletonStyle.backLinkSkeleton)}
       />
+
+      <div className={skeletonStyle.announcementsContainerSkeleton}>
+        <div className={skeletonStyle.announcementsHeaderSkeleton}>
+          <div
+            className={clsx(
+              skeletonStyle.skeleton,
+              skeletonStyle.announcementsTitleSkeleton
+            )}
+          />
+          <div
+            className={clsx(
+              skeletonStyle.skeleton,
+              skeletonStyle.announcementsToggleSkeleton
+            )}
+          />
+        </div>
+        <div className={skeletonStyle.announcementsListSkeleton}>
+          {announcementSkeletons.map((_, i) => (
+            <div
+              key={i}
+              className={clsx(
+                skeletonStyle.skeleton,
+                skeletonStyle.announcementSkeleton
+              )}
+              style={{ animationDelay: `${i * 100}ms` }}
+            />
+          ))}
+        </div>
+      </div>
 
       <div
         className={clsx(skeletonStyle.skeleton, skeletonStyle.titleSkeleton)}
